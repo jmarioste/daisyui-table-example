8db7335dc661efd538b58e96afc472ae119fc77a
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-10-06 22:22:12 +0800

    Implement column defs

---
+++ components/UserColumnDefs.tsx
@@ -0,0 +1,42 @@
+import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
+import { Person } from "../types/Person";
+
+const columnHelper = createColumnHelper<Person>();
+
+export const userColumnDefs: ColumnDef<Person, any>[] = [
+  columnHelper.accessor((row) => row.first_name, {
+    id: "first_name",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor((row) => row.last_name, {
+    id: "last_name",
+    cell: (info) => <span>{info.getValue()}</span>,
+    header: () => <span>Last Name</span>,
+  }),
+  columnHelper.accessor((row) => row.gender, {
+    id: "gender",
+    cell: (info) => <span>{info.getValue()}</span>,
+    header: () => <span>Gender</span>,
+  }),
+  columnHelper.accessor((row) => row.email, {
+    id: "email",
+    cell: (info) => <span>{info.getValue()}</span>,
+    header: () => <span>Email</span>,
+  }),
+  columnHelper.accessor((row) => row.ip_address, {
+    id: "ip_address",
+    cell: (info) => <span>{info.getValue()}</span>,
+    header: () => <span>IP Address</span>,
+  }),
+  columnHelper.accessor((row) => row.countryCode, {
+    id: "countryCode",
+    cell: (info) => <span>{info.getValue()}</span>,
+    header: () => <span>Country Code</span>,
+  }),
+  columnHelper.accessor((row) => row.city, {
+    id: "city",
+    cell: (info) => <span>{info.getValue()}</span>,
+    header: () => <span>City</span>,
+  }),
+];
